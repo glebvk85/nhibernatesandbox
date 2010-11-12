d3bc38ffcaea084a9599c7942b737594c0e641df
gorbach.alexey <gorbach.alexey@57010582-df8f-aeab-c389-756a87642e46>
2010-11-12 08:01:36 +0000

    LazyLoad assert added

--- branches/NH 3.0 New Features/NHibernateSandbox/Tests/LazyLoadFixture.cs
+++ branches/NH 3.0 New Features/NHibernateSandbox/Tests/LazyLoadFixture.cs
@@ -1,3 +1,4 @@
+using NHibernate;
 using NHibernateSandbox.Model;
 using NHibernateSandbox.Utils;
 using NUnit.Framework;
@@ -8,7 +9,7 @@ namespace NHibernateSandbox.Tests
     public class LazyLoadFixture : TestBase
     {
         [Test]
-        public void CanInsertPerson()
+        public void CanLazyLoadPhoto()
         {
             //Arrange
             var p = new Person() { Name = "BatMan", Photo = new byte[]{0, 1, 2, 3, 4, 5}};
@@ -21,6 +22,21 @@ namespace NHibernateSandbox.Tests
             //Assert
             Assert.That(actual.Name, Is.EqualTo(p.Name));
             Assert.That(actual.Photo, Is.EqualTo(p.Photo));
+        }
+
+        [Test]
+        public void LazyLoadPhoto()
+        {
+            //Arrange
+            var p = new Person() { Name = "BatMan", Photo = new byte[] { 0, 1, 2, 3, 4, 5 } };
+            Session.Save(p);
+            Session.Clear();
+
+            //Act
+            var actual = Session.Get<Person>(p.Id);
+
+            //Assert
+            Assert.That(NHibernateUtil.IsPropertyInitialized(actual, "Photo"), Is.False);
         }
     }
 }
